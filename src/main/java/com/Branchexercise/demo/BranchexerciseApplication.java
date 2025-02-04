dd1147f2f2eb084f4319d63fe5bbef9bb35b392a
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-02-04 11:25:49 +0100

    first commit

---
+++ src/main/java/com/Branchexercise/demo/BranchexerciseApplication.java
@@ -0,0 +1,13 @@
+package com.Branchexercise.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchexerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Branchexercise/demo/BranchexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Branchexercise.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchexerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
